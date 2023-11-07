55af88ab661a9dc1f7431188b00d52b9012ed274
vasanth241995 <86546052+vasanth241995@users.noreply.github.com>
2023-11-07 20:36:59 +0530

    Delete general_start_and_stop_scripts directory

--- general_start_and_stop_scripts/start_trillium.sh
+++
@@ -1,1 +0,0 @@
-nohup java -jar -Dtrillium_logger_path=/appl/trilapi/logs -Dserver.ssl.enabled-protocols=TLSv1.2 -Djasypt.encryptor.password=TRILLIUM_JASYPT_TOKEN -Dspring.profiles.active=qa trillium-api-1.0.0-SNAPSHOT.jar &
